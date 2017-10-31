5f56fb5d7d0c489a7a7d15a2fb3103acc308b614
Arjun Kakkar <arjunkakkar8@gmail.com>
2017-10-31 11:11:48 -0400

    Basic structural stuff

--- test.m
+++ test.m
@@ -1,1 +1,13 @@
-%test file for commit
+% Set seed
+rng(100)
+
+% Size of input data
+N = 100;
+% Size of compressed data
+n = 10;
+
+% Define starting function
+f = rand (1,N);
+% Store a random subset of points
+[fcomp, pos] = datasample(f, n, 'Replace', false);
+
