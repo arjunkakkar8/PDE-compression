df10782dfe468b6ed87c8cf59618afc3ea46665c
Arjun Kakkar <arjunkakkar8@gmail.com>
2017-11-08 15:35:48 -0500

    Fixed the Laplacian file. Removed loops, simplified matrix definition, created new plots. The function works much much better now.

--- Laplacian2D.m
+++ Laplacian2D.m
@@ -2,20 +2,20 @@
 % Laplacian operator
 
 % Import image
-img = imread('testChapin.jpg');
+img = imread('image.jpeg');
 % Store greyvalues of image
-imggray = rgb2gray(img);
+imggray = double(rgb2gray(img));
 
 % Dimensions of image
-width = size(imggray, 2);
-height = size(imggray, 1);
+width = size(imggray, 2);  % Must be greater than 2
+height = size(imggray, 1); % Must be greated than 2
 N = width * height;
 
 % Size of compressed data
 n = floor(N * 0.3);
 
 % Random greyvalues for now
-f = reshape(imggray,1,N);
+f = reshape(imggray',1,N);
 
 % Generate confidence vector of length N with n 1's
 c = zeros(1, N);
@@ -26,87 +26,56 @@ for i = 1:numel(knownIndex)
 end
 
 % Confidence diagonal matrix from c
-C = speye(N).*c;
-
+C = spdiags(c', 0, N, N);
+%C = spdiags([0,1,0,1,0,0]', 0, N, N);
 
 % Generate the Laplacian operator matrix A
 hx = 1;
 hy = 1;
 
-% Initialize A as N by N matrix
-A = zeros(N);
+% Define A matrix by defining 3 diagonals
 
-% Fill in matrix A by Mainberger(p.13)
-for i = 1:N
-    for j = 1:N
-        if neighbor(i, j, height, width) == 1
-            A(i,j) = 1./(hx)^2;
-            
-        elseif neighbor(i, j, height, width) == 2
-            A(i,j) = 1./(hy)^2;
-                
-        elseif i == j 
-            % right neighbor
-            if mod(i, width) < width
-                A(i,j) = A(i,j) - 1./(hx)^2;
-            end
-            % left neighbor
-            if mod(i, width) > 1
-                A(i,j) = A(i,j) - 1./(hx)^2;
-            end
-            % top neighbor
-            if floor(i/width) > 0
-                A(i,j) = A(i,j) - 1./(hy)^2;
-            end
-            % bottom neighbor
-            if floor(i/width) < height - 1
-                A(i,j) = A(i,j) - 1./(hy)^2;
-            end    
-            
-        else
-            A(i,j) = 0;
-        end
-    end
-end
+% First define the main diagonal 
+maindiag = [-1./(hx)^2-1./(hy)^2,...
+    repmat(-2./(hx)^2-1./(hy)^2, [1, width-2]),...
+    -1./(hx)^2-1./(hy)^2,...
+    repmat([-1./(hx)^2-2./(hy)^2,...
+    repmat(-2./(hx)^2-2./(hy)^2, [1, width-2]),...
+    -1./(hx)^2-2./(hy)^2], [1,height-2]),...
+    -1./(hx)^2-1./(hy)^2,...
+    repmat(-2./(hx)^2-1./(hy)^2, [1, width-2]),...
+    -1./(hx)^2-1./(hy)^2];
+
+% Define the diagonal with x-adjacencies
+xdiag = repmat([repmat(1./(hx)^2, [1,width-1]), 0], [1, height]);
+xdiagmod = repmat([0, repmat(1./(hx)^2, [1,width-1])], [1, height]);
+
+% Define the diagonal with y-adjacencies
+ydiag = repmat(1./(hy)^2, [1, N]);
+
+% Create the A matrix using these diagonals
+A = spdiags([ydiag' xdiag' maindiag' xdiagmod' ydiag'], [-width, -1, 0, 1, width], N, N);
 
 % Compute Mext
 Mext = C - (speye(N) - C) * A;
 
 % Compute u = inverse Mext * C * f
-u = inverse(Mext) * C * f.';
+u = Mext\(C * f.');
+
+% Create image matrix
+compimg = reshape(u, width, height)';
+
+% Split Image
+split = floor(width/2);
+splitimg = [imggray(:,1:split),compimg(:,width-split:width)];
 
 % Reconstruct image
-compimg = mat2gray(reshape(u, height, width));
-imshow(compimg)
+subplot(2, 2, 1)
+imshow(img)
+title('Original Image')
+subplot(2, 2, 2)
+imshow(mat2gray(compimg))
 title('Compressed Image')
-
-% returns 1 if i and j are neighboring cells
-% in h by w image
-function isNb = neighbor(i, j, h, w)
-      
-      % Reorder i, j s.t. i <= j
-      if i > j
-          temp = j;
-          j = i;
-          i = temp;
-      end
-      
-      % neighbors in y direction 
-      if i + h == j
-          isNb = 2;
-          
-      % neighbors in x direction
-      elseif i + 1 == j && mod(i, w) ~= 0
-          isNb = 1;
-          
-      else
-          isNb = 0;
-      end
-end
-
-
-
-
-
-
-
+subplot(2, 2, [3, 4])
+imshow(mat2gray(splitimg))
+title('Split Image')
